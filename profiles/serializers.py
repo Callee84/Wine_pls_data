fc0dbca2451e580b902f89d27c01d2949762a959
Callee84 <carl.g.holm@gmail.com>
2022-12-27 10:55:57 +0000

    Add login/out and IsOWner fields to profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from . models import WinePal
 
 class WinePalSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = WinePal
         fields = [
             'id', 'owner', 'created_on', 'updated_on', 'name',
-            'bio', 'img',
+            'bio', 'img', 'is_owner',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,33 +4,39 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import WinePal
 from .serializers import WinePalSerializer
+from wnpls_data.permissions import IsOwnerOrReadOnly
 
 
 class WinePalsList(APIView):
     def get(self, request):
         winepals = WinePal.objects.all()
-        serializer = WinePalSerializer(winepals, many=True)
+        serializer = WinePalSerializer(
+            winepals, many=True, context={'request': request})
         return Response(serializer.data)
 
 
 class WinePalDetails(APIView):
     serializer_class = WinePalSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             winepal = WinePal.objects.get(pk=pk)
+            self.check_object_permissions(self.request, winepal)
             return winepal
         except WinePal.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         winepal = self.get_object(pk)
-        serializer = WinePalSerializer(winepal)
+        serializer = WinePalSerializer(
+            winepal, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         winepal = self.get_object(pk)
-        serializer = WinePalSerializer(winepal, data=request.data)
+        serializer = WinePalSerializer(
+            winepal, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
