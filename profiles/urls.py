fcfc0f26a77d4fce7352caf0281927621b9f5e28
Callee84 <carl.g.holm@gmail.com>
2022-12-27 10:26:36 +0000

    Can now edit profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.WinePalsList.as_view()),
+    path('profiles/<int:pk>/', views.WinePalDetails.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import WinePal
@@ -9,3 +11,27 @@ class WinePalsList(APIView):
         winepals = WinePal.objects.all()
         serializer = WinePalSerializer(winepals, many=True)
         return Response(serializer.data)
+
+
+class WinePalDetails(APIView):
+    serializer_class = WinePalSerializer
+
+    def get_object(self, pk):
+        try:
+            winepal = WinePal.objects.get(pk=pk)
+            return winepal
+        except WinePal.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        winepal = self.get_object(pk)
+        serializer = WinePalSerializer(winepal)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        winepal = self.get_object(pk)
+        serializer = WinePalSerializer(winepal, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
