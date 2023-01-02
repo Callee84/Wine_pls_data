a32564b2db2f1814068a569748790c1dfa85e947
Callee84 <carl.g.holm@gmail.com>
2023-01-02 12:59:01 +0000

    try to fix bug with delpoyed api

--- wnpls_data/settings.py
+++ wnpls_data/settings.py
@@ -62,7 +62,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV'
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = [
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
+]
 
 
 # Application definition
